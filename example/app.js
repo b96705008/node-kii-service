27f2920b50e930b7067b1c69d950660aa02ae910
Tai-Yi Kuo <b96705008@gmail.com>
2015-10-14 23:16:51 +0800

    download file

--- example/app.js
+++ example/app.js
@@ -1,26 +1,52 @@
-var APP_ID = '',
-	APP_KEY ='',
-	CLIENT_ID = '',
-	CLIENT_SECRET = '';
+var kiiConf = require('./config');
 
 var path = require('path'),
 	Kii = require('../lib/kii-service'),
-	kiiService = new Kii(APP_ID, APP_KEY);
+	kiiService = new Kii(kiiConf.APP_ID, kiiConf.APP_KEY);
 
-// Load schemas
+/*
+ * Load schemas
+ */
 kiiService.loadSchemas(path.join(__dirname, 'schemas'));
 
-// BucketOpts can be 'string' or 'object'
-console.log(kiiService.getSchema('Account'));
-console.log(kiiService.getSchema({
+/*
+ * BucketOpts can be 'string' or 'object'
+ */
+kiiService.getSchema('Account')
+kiiService.getSchema({
 	scope: 'User',
 	bucketID: 'UserData'
-}));
+});
 
-// Set admin token
-//kiiService.setAdminToken(ADMIN_ACCESS_TOKEN);
+/*
+ * Set admin token 
+ */
+kiiService.setAdminToken(kiiConf.ADMIN_ACCESS_TOKEN);
 
-// Get token by client info
-kiiService.getAdminToken(CLIENT_ID, CLIENT_SECRET, function (err, data) {
-	console.log(data);
+/* 
+ *Get token by client info
+ */
+// kiiService.getAdminToken(kiiConf.CLIENT_ID, kiiConf.CLIENT_SECRET, function (err, data) {
+// 	console.log(data);
+// });
+
+// kiiService.queryObjects('ProfileImage', {
+// 	bucketQuery: {
+// 		clause: {type: 'all'}
+// 	}
+// }, function (err, data) {
+// 	console.log(err || data);
+// });
+
+/*
+ * Dowload Object
+ */
+var tmpPath = require('path').join(__dirname, 'tmp', 'file');
+
+kiiService.dowloadObject('FileBucket', 'ObjectID', tmpPath, function (err) {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log('download to', tmpPath);
+	}
 });
---
+++ example/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	APP_ID: '',
+	APP_KEY: '',
+	CLIENT_ID: '',
+	CLIENT_SECRET: '',
+	ADMIN_ACCESS_TOKEN: ''
+};
